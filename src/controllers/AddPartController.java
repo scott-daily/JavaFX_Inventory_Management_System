c74436a0b551b3209eba12e772158cd8c4a7a627
Scott Daily <scottjuggles@gmail.com>
2021-03-07 12:39:14 -0500

    Initial commit

---
+++ src/controllers/AddPartController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddPartController implements Initializable {
+    @Override
+    public void initialize (URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void toMain(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("To Main");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/models/Main.java
@@ -0,0 +1,25 @@
+package models;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+        primaryStage.setTitle("Inventory Management System");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
